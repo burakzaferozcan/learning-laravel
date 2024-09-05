d450fe6e687c5b2bec86441ff1eee6f843f8bdd1
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-09-05 22:06:51 +0300

    Calling css and js files with asset function

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset("assets/bootstrap/dist/css/bootstrap.min.css")}}">
+    <link rel="stylesheet" href="{{asset("assets/sweetalert2/dist/sweetalert2.min.css")}}">
+
     <title>Document</title>
     @yield("css")
 </head>
@@ -14,6 +17,13 @@
 @yield("icerik")
 
 @include("layouts.front.footer")
+
+<script src="{{asset("assets/bootstrap/dist/js/bootstrap.min.js")}}"></script>
+<script src="{{asset("assets/bootstrap/dist/js/bootstrap.bundle.min.js")}}"></script>
+<script src="{{asset("assets/jquery/dist/jquery.min.js")}}"></script>
+<script src="{{asset("assets/sweetalert2/dist/sweetalert2.all.min.js")}}"></script>
+
+
 
 @yield("js")
 </body>
