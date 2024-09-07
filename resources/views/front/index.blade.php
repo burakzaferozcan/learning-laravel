c20269e70bff3041476ed1b4e1750234b985e23c
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-09-07 21:47:56 +0300

    use of custom directive

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -5,13 +5,19 @@
 
 
 @section("icerik")
+    <form action="" method="POST">
+        @csrf
+        {{--        @method("DELETE")--}}
+        @auresMethod(`delete`)
+        <input type="text" name="fullname">
+        <button type="submit">Gönder</button>
+    </form>
     <hr>
     içerik alanı
     <hr>
     gelen yaş değeri : {{$age??"yaş değeri gönderilmedi"}}
     <hr>
     gelen isim değeri : {{$name??"isim değeri gönderilmedi"}}
-
     <hr>
 
 @endsection
